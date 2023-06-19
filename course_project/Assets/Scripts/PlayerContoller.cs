ba02bb7630b1626b2ada85a5b495f554202272ac
Rodion2020 <zaharov.rodion2000@yandex.ru>
2023-06-20 01:53:06 +0300

    five

--- course_project/Assets/Scripts/PlayerContoller.cs
+++ course_project/Assets/Scripts/PlayerContoller.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class PlayerContoller : MonoBehaviour
 {
     public float MovementSpeed = 3f;
-
-    // Start is called before the first frame update
-    void Start()
+    public GameObject SleepCanvas = null;
+    private Transform SleepArea;
+    public string SleepTag = "Sleep";
+    public float Distance = 1f;
+    private void Awake()
     {
         
+        SleepArea = GameObject.FindGameObjectWithTag(SleepTag).transform;
+
     }
+
+    //public void Sleep()
+    //{
+        
+    //}
+
+
+
+
 
     // Update is called once per frame
     void Update()
@@ -20,5 +34,22 @@ public class PlayerContoller : MonoBehaviour
         Vector3 translation = new Vector3(horiz, 0, vert);
         translation *= MovementSpeed * Time.deltaTime;
         transform.Translate(translation);
+
+
+        if (Vector3.Distance(SleepArea.position, transform.position) <= Distance)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                SleepCanvas.SetActive(true);
+                Time.timeScale = 0f;
+
+            }
+
+            else if (Input.GetKey(KeyCode.Q))
+            {
+                SleepCanvas.SetActive(false);
+                Time.timeScale = 1f;
+            }
+        }
     }
 }
